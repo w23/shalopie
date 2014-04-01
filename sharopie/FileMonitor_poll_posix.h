89d4c662ed2acd7cb1b36abe84ebf7d6505f8512
Ivan 'w23' Avdeev <me@w23.ru>
2014-04-01 21:52:06 +0700

    file polling for generic posix

---
+++ sharopie/FileMonitor_poll_posix.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <atomic>
+#include <pthread.h>
+#include <kapusha/core/String.h>
+
+class FileMonitor {
+public:
+  FileMonitor(const char *filename);
+  ~FileMonitor();
+  
+  void start();
+  
+  bool changed() const;
+  
+private:
+  void stop();
+  void thread_main();
+  static void *thread_func(void *monitor);
+  
+  kapusha::core::string_t filename_;
+  std::atomic_uint file_version_;
+  mutable unsigned int checked_version_;
+  
+  pthread_t thread_;
+  std::atomic_int cancel_;
+}; // class FileMonitor
